8e2a37180b09f22f6cc26370f46bb68fe5d85c37
stackdev37 <DouglasLuther0411@outlook.com>
2022-06-22 17:13:53 +0900

    h2 database connect

--- src/main/java/com/stackdev37/currencyexchangeservice/constants/ExchangeValue.java
+++ src/main/java/com/stackdev37/currencyexchangeservice/constants/ExchangeValue.java
@@ -1,46 +1,62 @@
 package com.stackdev37.currencyexchangeservice.constants;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.math.BigDecimal;
 
-public class ExchangeValue {
+@Entity
+@Table(name="Exchange_Value")
+public class ExchangeValue
+{
+    @Id
+    @Column(name="id")
     private Long id;
+    @Column(name="currency_from")
     private String from;
+    @Column(name="currency_to")
     private String to;
-    private BigDecimal conversionMuliple;
+    @Column(name="conversion_multiple")
+    private BigDecimal conversionMultiple;
+    @Column(name="port")
     private int port;
-
-    public ExchangeValue() {}
-
-    public ExchangeValue(Long id, String from, String to, BigDecimal conversionMuliple) {
+    public ExchangeValue()
+    {
+    }
+    //generating constructor using fields
+    public ExchangeValue(Long id, String from, String to, BigDecimal conversionMultiple)
+    {
         super();
         this.id = id;
         this.from = from;
         this.to = to;
-        this.conversionMuliple = conversionMuliple;
+        this.conversionMultiple = conversionMultiple;
     }
-
-    // generating getters
-    public int getPort() {
+    //generating getters
+    public int getPort()
+    {
         return port;
     }
-
-    public void setPort(int port) {
+    public void setPort(int port)
+    {
         this.port = port;
     }
-
-    public Long getId(){
+    public Long getId()
+    {
         return id;
     }
-
-    public String getFrom() {
+    public String getFrom()
+    {
         return from;
     }
-
-    public String getTo() {
+    public String getTo()
+    {
         return to;
     }
-
-    public BigDecimal getConversionMuliple() {
-        return conversionMuliple;
+    public BigDecimal getConversionMultiple()
+    {
+        return conversionMultiple;
     }
 }
+
